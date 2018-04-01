dc11053e6596ad679de58396549eb2f95c153b53
sergeidanilov@gmail.com <WF6m433ZxZ7LpU2g>
2018-04-01 16:23:33 -0700

    Added custom rootfs support both for load as iso image and iso installed on disk

--- bootstrap_rootfs/usr/lib64/dracut/modules.d/91custom-rootfs/copy_customrootfs.sh
+++ bootstrap_rootfs/usr/lib64/dracut/modules.d/91custom-rootfs/copy_customrootfs.sh
@@ -2,10 +2,28 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
+function extract_custom {
+  archives_dir=$1
+  cd $archives_dir
+  for archive in *-pulseaudio_custom_rootfs.tar.bz2; do
+    tar -xvf $archive -C $NEWROOT
+  done
+}
+
+# iso_dir exists when dracut boot args root=live:CDLABEL=PULSEAUDIO_LIVE
+# used to load iso image without installing it on flash/disc
+# it points to the directory where physical iso file present
 iso_dir=/run/initramfs/isoscan
-cd $iso_dir
-for archive in *-pulseaudio_custom_rootfs.tar.bz2; do
-  tar -xvf $archive -C $NEWROOT
-done
+
+# livecd_dir points to the location where iso disc contents mounted
+# consider it as root folder on flash when iso installed on flash.
+livecd_dir=/run/initramfs/live
+
+if [ -d $iso_dir ]; then
+  extract_custom $iso_dir
+else
+  extract_custom $livecd_dir
+fi
+
 
 

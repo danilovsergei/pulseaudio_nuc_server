6f6be8b336c76812f2989df844b77d9532cec946
sergeidanilov@gmail.com <WF6m433ZxZ7LpU2g>
2018-05-05 13:05:52 -0700

    allow pulse user to talk to systemd to restart pulseaudio service

--- src/bash_scripts/install_pulseaudio.sh
+++ src/bash_scripts/install_pulseaudio.sh
@@ -4,6 +4,11 @@ set -e
 id -u pulse > /dev/null 2>&1 || useradd -m -d /home/pulse pulse
 echo "pulse:12345678" | chpasswd
 
+# allow pulse user to talk to systemd to restart pulseaudio service
+auth_file=/etc/pam.d/system-auth
+allow_systemd="-session        optional        pam_systemd.so"
+grep -q "$allow_systemd" $auth_file || echo "$allow_systemd" >>  $auth_file
+
 chown -R pulse:pulse /home/pulse
 emerge -u pulseaudio
 
